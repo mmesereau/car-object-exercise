4cd3beb12c3a7a5b91be1960d892904299d9c1d1
Michael Mesereau <mmesereau1@gmail.com>
2016-07-27 09:32:55 -0600

    driving

--- car.js
+++ car.js
@@ -1,9 +1,31 @@
-
+'use strict';
 
 var Car = function (mpg) {
+  this.gallons = 0;
+  this.mpg = mpg;
+  this.mDriven = 0;
+  this.tripLog = [];
 };
 
 Car.prototype.fill = function (gallons) {
+  this.gallons += gallons;
 };
+
+Car.prototype.drive = function(miles) {
+  this.miles = miles;
+  this.gallons -= miles / this.mpg;
+  this.tripLog.push(this.miles + " miles");
+};
+
+Car.prototype.odometer = function() {
+  if (this.miles) {
+    this.mDriven += this.miles;
+  }
+  return this.mDriven;
+}
+
+Car.prototype.trips = function() {
+  return this.tripLog;
+}
 
 module.exports = Car;
